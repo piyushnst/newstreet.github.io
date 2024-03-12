ef2d02649d98d0d1215f81e20fd828fd91129519
piyushnst <piyush.gupta@newstreettech.com>
2024-03-12 15:10:01 +0530

    atlas

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb://localhost:27017/nstapi");
+    const conn = await mongoose.connect("mongodb+srv://piyushgupta:nicolatesla@cluster0.5zljmkl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
     console.log(`Connected to MongoDB Database ${conn.connection.host}`);
   } catch (error) {
     console.log(`Error in =MongoDB ${error}`);
